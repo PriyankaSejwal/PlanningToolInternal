2810b264d5491a03d056180ac7bc54d512ff0431
PriyankaSejwal <72335021+PriyankaSejwal@users.noreply.github.com>
2023-01-03 11:24:33 +0530

    Update tabletocsv.js

--- tabletocsv.js
+++ tabletocsv.js
@@ -21,40 +21,40 @@ function exportToExcel() {
   var elt2 = document.getElementById("tbl2");
   var elt3 = document.getElementById("tbl3");
   var elt4 = document.getElementById("tbl4");
-  var rows = [];
-  rows.push(["LINK SETTINGS"]);
+  var rows = [[],[],[]];
   for (var i = 0; i < elt.rows.length; i++) {
     column1 = elt.rows[i].cells[0].innerText;
     column2 = elt.rows[i].cells[1].innerText;
     column3 = elt.rows[i].cells[2].innerText;
-    //   column3 = elt.rows[i].cells[2].innerHTML;
-    //   column4 = elt.rows[i].cells[3].innerHTML;
 
-    rows.push([column1, column2, column3]);
+    rows[0].push(column1);
+    rows[1].push(column2);
+    rows[2].push(column3);
   }
-  rows.push([""], [""], ["LINK STATS"]);
-  for (var i = 0; i < elt2.rows.length; i++) {
+  for (var i = 1; i < elt2.rows.length; i++) {
     column1 = elt2.rows[i].cells[0].innerText;
     column2 = String(elt2.rows[i].cells[1].innerText);
     column3 = String(elt2.rows[i].cells[2].innerText);
-    // column4 = elt2.rows[i].cells[3].innerText;
 
-    rows.push([column1, column2, column3]);
+    rows[0].push(column1);
+    rows[1].push(column2);
+    rows[2].push(column3);
   }
-  rows.push([""], [""], ["Site A"]);
+  
   for (var i = 0; i < elt3.rows.length; i++) {
     column1 = elt3.rows[i].cells[0].innerText;
     column2 = elt3.rows[i].cells[1].innerText;
 
-    rows.push([column1, column2]);
+    rows[0].push(column1);
+    rows[1].push(column2);
   }
 
-  rows.push([""], [""], ["Site B"]);
   for (var i = 0; i < elt4.rows.length; i++) {
     column1 = elt4.rows[i].cells[0].innerText;
     column2 = elt4.rows[i].cells[1].innerText;
 
-    rows.push([column1, column2]);
+    rows[0].push(column1);
+    rows[1].push(column2);
   }
   csvContent = "data:text/csv;charset=utf-8,";
   rows.forEach(function (rowArray) {
@@ -67,7 +67,6 @@ function exportToExcel() {
   link.setAttribute("href", encodedUri);
   link.setAttribute("download", "Link_Report.csv");
   document.body.appendChild(link);
-  /* download the data file named "Stock_Price_Report.csv" */
   link.click();
   // Removing the created child a in the body.
   document.body.removeChild(link);
